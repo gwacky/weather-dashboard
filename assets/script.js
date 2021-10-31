b7c02a760ee51dfbf35ff4fc4fd9350a094f6516
gwacky <gracalynntrammell@gmail.com>
2021-10-31 12:36:29 -0500

    searched city weather and forecast html updated, variabled added into javascript

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,8 @@
 var previousSearches = document.getElementById("previousSearches");
 var searchInput = document.getElementById("searchInput");
 var searchButton = document.getElementById("searchButton");
+
+var searchedCity = document.getElementById("searchedCity");
+var searchedCityWeather = document.getElementById("searchedCityWeather");
+var forecastTitle = document.getElementById("forecastTitle");
+var forcastWeather = document.getElementById("forecastWeather");
